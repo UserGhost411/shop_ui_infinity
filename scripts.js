9316fcc9ce3122dde62820b9ba4ed5ce0a69e50f
UserGhost411 <UserGhost411@yahoo.com>
2024-04-02 08:31:57 +0700

    update

--- scripts.js
+++ scripts.js
@@ -1,64 +1,22 @@
 let table = []
 let selectedWindow = "none"
-let shopItems =[]
+let shopItems = []
 let tabindexx = 0
 let cartitems = []
+$(".main_card").hide()
 window.addEventListener('message', function (event) {
 	switch (event.data.action) {
-		case 'init_data':
-			if (event.data.timeout_warga > 1) {
-				$("#testid_timeout").removeClass("d-none")
-			}
-
-			timeout_warga = event.data.timeout_warga
-			break
-		case 'uppershow':
-			if (event.data.hide) {
-				$("#upper_id").addClass("d-none")
-			} else {
-				$("#upper_id").removeClass("d-none")
-			}
-			break
-		case 'loading_data':
-			if (selectedWindow == "none") {
-				$('#menu').html(`
-					<div class="d-flex justify-content-center flex-column align-items-center">
-						<span class="load"></span>
-						<br>
-						<div style="font-size: 40px; color: #fff;">Loading Data...</div>
-					</div>
-				`);
-				$("#menu").fadeIn();
-				$(".main_card").fadeIn();
-				selectedWindow = "loading_data";
-			}
-			break
-		case 'show_about':
-			$('#menu').html(`
-						<h1>Selamat datang di Infinity Server</h1>
-						<p>Server Owner and Administrator : Skies (InfamousGamer)<Br>Lead Dev: UserGhost411<br><br>NUI can also send calls back to the game using so-called 'NUI callbacks'. These are currently only fully supported in Lua, other languages can be used but need a bit of a tricky workaround as these predate function references in codegen.
-
-						Generally, you'll use the RegisterNUICallback function in Lua, and the REGISTER_NUI_CALLBACK_TYPE native along with an event handler in other languages.</p>
-					`);
-			$("#menu").fadeIn();
+		case 'show_shop':
+			// event.data
+			additem("pistol", "Pistol", "https://picsum.photos/200/100?8", 150000, 10, "Lainnya")
+			loadshop()
 			$(".main_card").fadeIn();
-			selectedWindow = "show_about";
-			break
-		case 'update_info_player':
-			$("#info_player").show()
-			$("#info_player").html(`ID: ${event.data.id}  |  Job: ${event.data.job} (${event.data.grade})`)
-			break
-		case 'notif_sound':
-			let audio = new Audio('atm.mp3');
-			audio.play();
-			break
-		case 'blind':
-			if (event.data.state == 1) {
-				$(document.body).addClass("bg-dark")
-			} else {
-				$(document.body).removeClass("bg-dark")
-			}
-			break
+			selectedWindow = event.data.action.toString();
+		break
+		case 'close':
+			$(".main_card").fadeOut()
+			selectedWindow = "none"
+		break
 	}
 });
 function secondsToTime(seconds) {
@@ -93,17 +51,14 @@ function secondsToTime(seconds) {
 
 	return result[0] + " " + (result[1] != undefined ? result[1] : ""); // Remove the trailing ", "
 }
-function closewindow(){
+function closewindow() {
 	$.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
-				action: "close",
+		action: "close",
 	}));
 	selectedWindow = "none"
-	// $("#menu").fadeOut();
 	$(".main_card").fadeOut();
 }
-// Close ESC Key
 $(document).ready(function () {
-	// $("#info_player").hide()
 	document.onkeyup = function (data) {
 		if (data.which == 27) {
 			closewindow()
@@ -114,42 +69,41 @@ $(document).ready(function () {
 		$(".searchable div").filter(function () {
 			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 		});
-		
 	});
-	additem("bebek_bakar","Bebek Bakar","https://picsum.photos/200/100?1",20000,10,"Makanan")
-	additem("bebek_goreng","Bebek Goreng","https://picsum.photos/200/100?2",15000,10,"Makanan")
-	additem("bebek_kukus","Bebek Kukus","https://picsum.photos/200/100?3",15000,10,"Makanan")
-	additem("es_teh","Es Teh","https://picsum.photos/200/100?4",3000,10,"Minuman")
-	additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?5",5000,10,"Minuman")
-	additem("es_cola","Es Cola","https://picsum.photos/200/100?6",5000,10,"Minuman")
-	additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?73",4000,10,"Minuman")
-	additem("es_teh","Es Teh","https://picsum.photos/200/100?42",3000,10,"Minuman")
-	additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?52",5000,10,"Minuman")
-	additem("es_cola","Es Cola","https://picsum.photos/200/100?62",5000,10,"Minuman")
-	additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?72",4000,10,"Minuman")
-	additem("es_teh","Es Teh","https://picsum.photos/200/100?42",3000,10,"Minuman")
-	additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?53",5000,10,"Minuman")
-	additem("es_cola","Es Cola","https://picsum.photos/200/100?64",5000,10,"Minuman")
-	additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?47",4000,10,"Minuman")
-	additem("es_teh","Es Teh","https://picsum.photos/200/100?44",3000,10,"Minuman")
-	additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?54",5000,10,"Minuman")
-	additem("es_cola","Es Cola","https://picsum.photos/200/100?65",5000,10,"Minuman")
-	additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?75",4000,10,"Minuman")
-	additem("pistol","Pistol","https://picsum.photos/200/100?8",150000,10,"Lainnya")
-	loadshop()
+	// additem("bebek_bakar","Bebek Bakar","https://picsum.photos/200/100?1",20000,10,"Makanan")
+	// additem("bebek_goreng","Bebek Goreng","https://picsum.photos/200/100?2",15000,10,"Makanan")
+	// additem("bebek_kukus","Bebek Kukus","https://picsum.photos/200/100?3",15000,10,"Makanan")
+	// additem("es_teh","Es Teh","https://picsum.photos/200/100?4",3000,10,"Minuman")
+	// additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?5",5000,10,"Minuman")
+	// additem("es_cola","Es Cola","https://picsum.photos/200/100?6",5000,10,"Minuman")
+	// additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?73",4000,10,"Minuman")
+	// additem("es_teh","Es Teh","https://picsum.photos/200/100?42",3000,10,"Minuman")
+	// additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?52",5000,10,"Minuman")
+	// additem("es_cola","Es Cola","https://picsum.photos/200/100?62",5000,10,"Minuman")
+	// additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?72",4000,10,"Minuman")
+	// additem("es_teh","Es Teh","https://picsum.photos/200/100?42",3000,10,"Minuman")
+	// additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?53",5000,10,"Minuman")
+	// additem("es_cola","Es Cola","https://picsum.photos/200/100?64",5000,10,"Minuman")
+	// additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?47",4000,10,"Minuman")
+	// additem("es_teh","Es Teh","https://picsum.photos/200/100?44",3000,10,"Minuman")
+	// additem("es_jeruk","Es Jeruk","https://picsum.photos/200/100?54",5000,10,"Minuman")
+	// additem("es_cola","Es Cola","https://picsum.photos/200/100?65",5000,10,"Minuman")
+	// additem("es_nutrisari","Es Nutrisari","https://picsum.photos/200/100?75",4000,10,"Minuman")
+	// additem("pistol","Pistol","https://picsum.photos/200/100?8",150000,10,"Lainnya")
+	// loadshop()
 });
 
-function additem(id,name,img,price,stock,category){
+function additem(id, name, img, price, stock, category) {
 	shopItems.push({
-		id:id,
-		name:name,
-		img:img,
-		stock:stock,
-		price:price,
-		category:category
+		id: id,
+		name: name,
+		img: img,
+		stock: stock,
+		price: price,
+		category: category
 	})
 }
-function createItemObject(item){
+function createItemObject(item) {
 	return `<div class="col-3 mb-2">
 	<div class="card border border-secondary">
 		<img src="${item.img}" class="card-img-top" alt="...">
@@ -161,35 +115,35 @@ function createItemObject(item){
 	</div>
 </div>`
 }
-function humanification(data){
-	return capitalizeEachWord(data.replace("_"," ").replace("-"," "))
+function humanification(data) {
+	return capitalizeEachWord(data.replace("_", " ").replace("-", " "))
 }
-function idnification(data){
-	return (data.replace(" ","_").replace(",","").replace(".","")).toLowerCase()
+function idnification(data) {
+	return (data.replace(" ", "_").replace(",", "").replace(".", "")).toLowerCase()
 }
-function createCategoryObject(category){
+function createCategoryObject(category) {
 	tabindexx++;
 	const idread = idnification(category)
-	return `<div class="tab-pane fade ${(tabindexx==0?"show active":"")}" id="nav-${idread}" role="tabpanel" tabindex="0">
+	return `<div class="tab-pane fade ${(tabindexx == 0 ? "show active" : "")}" id="nav-${idread}" role="tabpanel" tabindex="0">
 		<div class="row searchable" id="content_cat_${idread}">
 	</div>
 </div>`
 }
-function createCategoryNavObject(category){
-	
+function createCategoryNavObject(category) {
+
 	const humanread = humanification(category)
 	const idread = idnification(category)
 	return `<button class="nav-link" id="nav-${idread}-tab" data-bs-toggle="tab"
 	data-bs-target="#nav-${idread}" type="button" role="tab">${humanread}</button>`
 }
-function capitalizeEachWord(string){
-    const str_arr = string.split(' ')
-    for(i = 0; i < str_arr.length; i++){
-        str_arr[i] = str_arr[i][0].toUpperCase() + str_arr[i].slice(1)
-    }
-    return str_arr.join(' ')
+function capitalizeEachWord(string) {
+	const str_arr = string.split(' ')
+	for (i = 0; i < str_arr.length; i++) {
+		str_arr[i] = str_arr[i][0].toUpperCase() + str_arr[i].slice(1)
+	}
+	return str_arr.join(' ')
 }
-function loadshop(){
+function loadshop() {
 	tabindexx = 0
 	cartitems = []
 	let categorys = []
@@ -197,7 +151,7 @@ function loadshop(){
 	let navcontent = []
 	let itemcontent = []
 	for (const item of shopItems) {
-		if (!categorys.includes(item.category)){
+		if (!categorys.includes(item.category)) {
 			categorys.push(item.category)
 		}
 		// itemcontent.push(createItemObject(item.id,item.name,item.img))
@@ -206,7 +160,7 @@ function loadshop(){
 		tabcontent.push(createCategoryObject(cat))
 		navcontent.push(createCategoryNavObject(cat))
 	}
-	
+
 	$("#nav-tabContent").html(tabcontent.join(""))
 	$("#nav-tab").html(navcontent.join(""))
 	for (const item of shopItems) {
@@ -216,48 +170,48 @@ function loadshop(){
 	$(`#nav-${idnification(categorys[0])}-tab`).click()
 	refreshCart()
 }
-function stocksafe(id,val){
+function stocksafe(id, val) {
 	for (const i of shopItems) {
-		if (i.id == id){
+		if (i.id == id) {
 			return (i.stock >= val)
 		}
 	}
 	return false
 }
-function addstock(id,add,all){
+function addstock(id, add, all) {
 	let tmp = []
-	if (isOnCart(id)){
-		if (add == false && all){
+	if (isOnCart(id)) {
+		if (add == false && all) {
 			for (const ca of cartitems) {
 				if (ca.id != id) tmp.push(ca)
 			}
 			cartitems = tmp
 			return refreshCart()
-		}else if(add){
+		} else if (add) {
 			for (const ca of cartitems) {
-				if (ca.id == id){
+				if (ca.id == id) {
 					tmp.push({
-						id:ca.id,
-						qty:(ca.qty + (stocksafe(id,(ca.qty + 1))?1:0))
+						id: ca.id,
+						qty: (ca.qty + (stocksafe(id, (ca.qty + 1)) ? 1 : 0))
 					})
-				}else{
+				} else {
 					tmp.push(ca)
-				} 
+				}
 			}
 			cartitems = tmp
 			return refreshCart()
-		}else if(!add){
+		} else if (!add) {
 			for (const ca of cartitems) {
 				if (ca.id == id) {
 					if (ca.qty == 1) {
-						return addstock(id,false,true)
-					}else{
+						return addstock(id, false, true)
+					} else {
 						tmp.push({
-							id:ca.id,
-							qty:(ca.qty - 1)
+							id: ca.id,
+							qty: (ca.qty - 1)
 						})
 					}
-				}else{
+				} else {
 					tmp.push(ca)
 				}
 			}
@@ -266,11 +220,11 @@ function addstock(id,add,all){
 		}
 	}
 }
-function duit(a){
+function duit(a) {
 	return a.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
 }
-function refreshCart(){
-	if (cartitems.length > 0){
+function refreshCart() {
+	if (cartitems.length > 0) {
 
 		$("#cart_content").html(`<p>Your Cart:</p><div id="cart_items"></div><div id="cart_detail"></div>`)
 		let total_price = 0
@@ -278,9 +232,9 @@ function refreshCart(){
 		for (const cart of cartitems) {
 			for (const item of shopItems) {
 				if (item.id == cart.id) {
-					
-						total_price += (cart.qty * item.price)
-						cart_items.push(`<div class="my-2 p-1 bg-dark border border-1 rounded-2">
+
+					total_price += (cart.qty * item.price)
+					cart_items.push(`<div class="my-2 p-1 bg-dark border border-1 rounded-2">
 						<div class="mx-1 d-flex flex-row align-items-center justify-content-between">
 							<div class="flex-grow-1">
 								${item.name} 
@@ -297,7 +251,7 @@ function refreshCart(){
 							</div>
 						</div>
 					  </div>`)
-					
+
 					break
 				}
 			}
@@ -307,36 +261,36 @@ function refreshCart(){
 		<div class="col-6"><button class="btn btn-success w-100" onclick="closewindow()">Buy</button></div>
 		<div class="col-6"><button class="btn btn-danger w-100" onclick="closewindow()">Cancel</button></div>
 		</div> `)
-		
-	}else{
+
+	} else {
 		$("#cart_content").html(`<center class="my-3"><h3>Empty Cart</h3><i class="fas fa-shopping-basket fa-3x"></i></center> <hr> <button class="btn btn-danger w-100" onclick="closewindow()">Cancel</button>`)
 	}
-	
+
 }
-function isOnCart(id){
+function isOnCart(id) {
 	for (const c of cartitems) {
-		if (c.id == id){
+		if (c.id == id) {
 			return true
 		}
 	}
 	return false
 }
-function addtocart(btn,id){
-	if (!isOnCart(id)){
+function addtocart(btn, id) {
+	if (!isOnCart(id)) {
 		for (const item of shopItems) {
 			if (item.id == id) {
 				if (item.stock > 0) {
 					cartitems.push({
-						id:item.id,
-						qty:1,
+						id: item.id,
+						qty: 1,
 					})
 					refreshCart()
 				}
 				break
 			}
 		}
-	}else{
-		addstock(id,true,false)
+	} else {
+		addstock(id, true, false)
 	}
-	
+
 }
